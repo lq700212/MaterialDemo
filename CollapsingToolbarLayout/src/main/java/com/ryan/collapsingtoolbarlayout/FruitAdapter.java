573ecfac6f6003941103e4519cfd8ecd42e266f7
ryan.lei <leiqi@baiqishi.com>
2019-08-07 16:48:03 +0800

    完成CollapsingToolbarLayout Demo

---
+++ CollapsingToolbarLayout/src/main/java/com/ryan/collapsingtoolbarlayout/FruitAdapter.java
@@ -0,0 +1,73 @@
+package com.ryan.collapsingtoolbarlayout;
+
+import android.content.Context;
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.CardView;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+
+import java.util.List;
+
+public class FruitAdapter extends RecyclerView.Adapter<FruitAdapter.ViewHolder> {
+
+    private Context mContext;
+    private List<Fruit> mFruitList;
+
+    static class ViewHolder extends RecyclerView.ViewHolder {
+        CardView cardView;
+        TextView name;
+        ImageView imageId;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            cardView = (CardView) itemView;
+            name = (TextView) itemView.findViewById(R.id.fruit_name);
+            imageId = (ImageView) itemView.findViewById(R.id.fruit_image);
+        }
+    }
+
+    public FruitAdapter(List<Fruit> fruitList) {
+        mFruitList = fruitList;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+        if (mContext == null) {
+            mContext = viewGroup.getContext();
+        }
+        View view = LayoutInflater.from(mContext).inflate(R.layout.fruit_item, viewGroup, false);
+        final ViewHolder viewHolder = new ViewHolder(view);
+        viewHolder.cardView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int position = viewHolder.getAdapterPosition();
+                Fruit fruit = mFruitList.get(position);
+                Intent intent = new Intent(mContext, FruitActivity.class);
+                intent.putExtra(FruitActivity.FRUIT_NAME, fruit.getName());
+                intent.putExtra(FruitActivity.FRUIT_IMAGE_ID, fruit.getImageId());
+                mContext.startActivity(intent);
+            }
+        });
+        return viewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder viewHolder, int i) {
+        Fruit fruit = mFruitList.get(i);
+        viewHolder.name.setText(fruit.getName());
+        Glide.with(mContext).load(fruit.getImageId()).into(viewHolder.imageId);
+    }
+
+    @Override
+    public int getItemCount() {
+        return mFruitList.size();
+    }
+}
